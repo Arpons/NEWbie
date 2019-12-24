d2c37e4680338948b586d8c62dd40ce18b967b54
Arpons <49786729+Arpons@users.noreply.github.com>
2019-12-24 17:18:19 +0900

    소수 구하기

--- 20191224_1.py
+++ 20191224_1.py
@@ -1,8 +1,20 @@
-def gcd():
-    num = int(input("정수를 입력해주세요 :"))
-    a = []
-    for i in range(1, num+1):
-        if num % i == 0:
-            a.append(i)
-    print(a == 2)
-gcd()
+num = int(input("정수를 입력하세요 : "))
+def first():
+    for i in range(2, num + 1):
+        cheak = True
+        for j in range(2, i):
+            if i % j == 0:
+                cheak = False
+            break
+        if cheak:
+            print(i, end=" ")
+
+def second():
+    num = int(input("정수를 입력하세요 : "))
+    for i in range(1, num + 1):
+        count = 0
+        for j in range(1, i + 1):
+            if i%j == 0:
+                count += 1
+        if count == 2:
+            print(j)
